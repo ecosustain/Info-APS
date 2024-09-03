450c5f159deeb59d557e57bbbc7a3b52bb81dd8f
Daniel Schulz <danielfs0@hotmail.com>
2024-09-03 19:09:55 -0300

    Add mais paginas

---
+++ painel/paginas/mapa.py
@@ -0,0 +1,9 @@
+from dash import dcc, html
+
+layout = html.Div(
+    [
+        html.H1("Mapa de Saúde"),
+        html.P("Conteúdo do Mapa aqui..."),
+        # Adicione o código específico para o mapa aqui
+    ]
+)
