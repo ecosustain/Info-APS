3abf1cb7c138bcaf07e9937f9c24e17c3116feb7
Leonardo Gomes <leonardodasigomes@gmail.com>
2024-09-17 15:56:32 -0300

    Adjust initil configs on dashboard

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd383c8b7de1e00c"; }
+/******/ 		__webpack_require__.h = function() { return "c9f53bf15e922dd7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
