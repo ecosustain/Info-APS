a6d2ff23ae58732dadb6cba078762ab0007db29d
Leonardo Gomes <leonardodasigomes@gmail.com>
2024-09-23 12:22:38 -0300

    Add style and behaviour on state map

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -15,46 +15,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-      <BrazilMap/>
-        {/* <img
-          className={styles.logo}
-          src={Brazil}
-          alt="Next.js logo"
-          width={180}
-          height={38}
-        /> */}
-        <ol>
-          <li onMouseOver={() => console.log("leo")}>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <BrazilMap />
       </main>
       <footer className={styles.footer}>
         <a
