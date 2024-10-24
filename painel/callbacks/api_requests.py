fae8f2e34acce3043a7566f8dfde72e7261968f3
Daniel Schulz <danielfs0@hotmail.com>
2024-10-24 05:22:41 -0300

    Refatoradas requisicoes

--- painel/callbacks/api_requests.py
+++ painel/callbacks/api_requests.py
@@ -4,108 +4,83 @@ import pandas as pd
 import requests
 from callbacks.utils import get_code_regiao, get_ibge_code
 
+API_URL = "https://dash-saude-mongo.elsvital.dev/api/v1"
 
-def get_municipios(estado):
-    """Função para obter os municipios de um estado"""
-    url = f"https://dash-saude-mongo.elsvital.dev/api/v1/cities/{estado}"
+
+def make_request(url):
+    """Função para fazer uma requisição à API"""
     headers = {"accept": "application/json"}
 
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        cities = response.json()
-        return cities
+        return response.json()
     else:
         print(response.status_code)
         print(response.text)
         return None
+
+
+def get_municipios(estado):
+    """Função para obter os municipios de um estado"""
+    url = f"{API_URL}/cities/{estado}"
+
+    return make_request(url)
 
 
 def get_atendimentos(estado, regiao, municipio):
     """Função para obter os dados de atendimentos"""
-    url = "https://dash-saude-mongo.elsvital.dev/api/v1/atendimentos"
+    url = f"{API_URL}/atendimentos"
     if estado is not None:
-        url = f"https://dash-saude-mongo.elsvital.dev/api/v1/atendimentos/states/{estado}"
+        url = f"{API_URL}/atendimentos/states/{estado}"
     if regiao is not None and estado is not None and municipio is None:
         regiao_code = get_code_regiao(estado, regiao)
-        url = f"https://dash-saude-mongo.elsvital.dev/api/v1/atendimentos/regions/{regiao_code}"
+        url = f"{API_URL}/atendimentos/regions/{regiao_code}"
     if municipio is not None and estado is not None:
         ibge_code = get_ibge_code(estado, municipio)
-        url = f"https://dash-saude-mongo.elsvital.dev/api/v1/atendimentos/cities/{ibge_code}"
+        url = f"{API_URL}/atendimentos/cities/{ibge_code}"
     print("Fazendo request para:", url)
 
-    headers = {"accept": "application/json"}
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(response.status_code)
-        print(response.text)
-        return None
+    return make_request(url)
 
 
 def get_altas(estado, regiao, municipio):
     """Função para obter os dados de altas"""
-    url = "https://dash-saude-mongo.elsvital.dev/api/v1/altas"
+    url = f"{API_URL}/altas"
     if estado is not None:
-        url = f"https://dash-saude-mongo.elsvital.dev/api/v1/altas/states/{estado}"
+        url = f"{API_URL}/altas/states/{estado}"
     if regiao is not None and estado is not None and municipio is None:
         regiao_code = get_code_regiao(estado, regiao)
-        url = f"https://dash-saude-mongo.elsvital.dev/api/v1/altas/regions/{regiao_code}"
+        url = f"{API_URL}/altas/regions/{regiao_code}"
     if municipio is not None and estado is not None:
         ibge_code = get_ibge_code(estado, municipio)
-        url = f"https://dash-saude-mongo.elsvital.dev/api/v1/altas/cities/{ibge_code}"
+        url = f"{API_URL}/altas/cities/{ibge_code}"
     print("Fazendo request para:", url)
-    headers = {"accept": "application/json"}
 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(response.status_code)
-        print(response.text)
-        return None
+    return make_request(url)
 
 
 def get_encaminhamentos(estado, regiao, municipio):
     """Função para obter os dados de encaminhamentos"""
-    url = "https://dash-saude-mongo.elsvital.dev/api/v1/encaminhamentos"
+    url = f"{API_URL}/encaminhamentos"
     if estado is not None:
-        url = f"https://dash-saude-mongo.elsvital.dev/api/v1/encaminhamentos/states/{estado}"
+        url = f"{API_URL}/encaminhamentos/states/{estado}"
     if regiao is not None and estado is not None and municipio is None:
         regiao_code = get_code_regiao(estado, regiao)
-        url = f"https://dash-saude-mongo.elsvital.dev/api/v1/encaminhamentos/regions/{regiao_code}"
+        url = f"{API_URL}/encaminhamentos/regions/{regiao_code}"
     if municipio is not None and estado is not None:
         ibge_code = get_ibge_code(estado, municipio)
-        url = f"https://dash-saude-mongo.elsvital.dev/api/v1/encaminhamentos/cities/{ibge_code}"
+        url = f"{API_URL}/encaminhamentos/cities/{ibge_code}"
 
-    headers = {"accept": "application/json"}
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(response.status_code)
-        print(response.text)
-        return None
+    return make_request(url)
 
 
 def get_anos(num):
     """Retorna uma lista com os anos a serem utilizados"""
-    url = f"https://dash-saude-mongo.elsvital.dev/api/v1/years"
-    headers = {"accept": "application/json"}
-    response = requests.get(url, headers=headers)
+    url = f"{API_URL}/years"
 
-    if response.status_code == 200:
-        anos = response.json()
-        anos = anos[-num:]
-        anos.sort(reverse=True)
-        return anos
-    else:
-        print(response.status_code)
-        print(response.text)
-        return None
+    anos = make_request(url)
+    anos = anos[-num:]
+    anos.sort(reverse=True)
+
+    return anos
