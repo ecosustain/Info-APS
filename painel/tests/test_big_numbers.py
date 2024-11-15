33fc0e9bcb6d7018e8bc2b9ed5f2ac572602b959
Daniel Schulz <danielfs0@hotmail.com>
2024-11-15 20:14:05 -0300

    Add test sp

--- painel/tests/test_big_numbers.py
+++ painel/tests/test_big_numbers.py
@@ -1,16 +1,19 @@
 import pytest
 from dash.testing.application_runners import import_app
 from unittest.mock import patch
-
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 
 @pytest.fixture
 def app():
-    # Importa o arquivo principal app.py, onde o Dash é instanciado
-    app = import_app("painel.app")  # Substitua pelo caminho completo do app
+    # Importar o aplicativo do arquivo app.py
+    app = import_app("app")
     return app
 
+cadastro = round(379197375 / 12)
+
 @pytest.mark.parametrize("expected_output", [
-    (['', '', '356.9M', 2028, 1232])  # Exemplo de teste
+    (['75.0M', 2372, round(50039489/cadastro*1000)])  # Exemplo de teste
 ])
 @patch("callbacks.initial_informations.big_number_ii.get_selected_year", return_value=2023)
 def test_update_big_numbers_atend(
@@ -19,28 +22,20 @@ def test_update_big_numbers_atend(
     # Inicie o servidor do app e espere até ele carregar
     dash_duo.start_server(app)
 
-    # Adicione uma espera para o carregamento dos indicadores
-    dash_duo.wait_for_element("#indicador-atend-brasil", timeout=10)
-    dash_duo.wait_for_element("#indicador-atend-estado", timeout=10)
-    dash_duo.wait_for_element("#total-atendimentos", timeout=10)
-    dash_duo.wait_for_element("#normalizado-atendimentos", timeout=10)
-    dash_duo.wait_for_element("#big-medicos", timeout=10)
+    # Selecionar o estado de São Paulo (SP) no dropdown
+    dropdown = dash_duo.find_element(".Select-input input")
+    dropdown.send_keys("SP")
+    dropdown.send_keys(Keys.ENTER)
 
     # Esperar até que o indicador de carregamento desapareça
     dash_duo.wait_for_element("#loading-graphics", timeout=10)
     while dash_duo.find_element("#loading-graphics").is_displayed():
         dash_duo.wait_for_element("#loading-graphics", timeout=1)
 
-    # Verifique os valores dos elementos usando o método `get_property`
-    brasil_value = dash_duo.find_element("#indicador-atend-brasil").text
-    estado_value = dash_duo.find_element("#indicador-atend-estado").text
     total_atendimentos_value = dash_duo.find_element("#total-atendimentos").text
     normalizado_atendimentos_value = dash_duo.find_element("#normalizado-atendimentos").text
     big_medicos_value = dash_duo.find_element("#big-medicos").text
 
-    # Verifique se os valores dos elementos correspondem aos valores esperados
-    assert str(brasil_value) == str(expected_output[0]), f"Valor do indicador Brasil esperado: {expected_output[0]}, mas encontrou: {brasil_value}"
-    assert str(estado_value) == str(expected_output[1]), f"Valor do indicador Estado esperado: {expected_output[1]}, mas encontrou: {estado_value}"
-    assert str(total_atendimentos_value) == str(expected_output[2]), f"Valor de Total Atendimentos esperado: {expected_output[2]}, mas encontrou: {total_atendimentos_value}"
-    assert str(normalizado_atendimentos_value) == str(expected_output[3]), f"Valor de Normalizado Atendimentos esperado: {expected_output[3]}, mas encontrou: {normalizado_atendimentos_value}"
-    assert str(big_medicos_value) == str(expected_output[4]), f"Valor de Big Médicos esperado: {expected_output[4]}, mas encontrou: {big_medicos_value}"
+    assert str(total_atendimentos_value) == str(expected_output[0]), f"Valor de Total Atendimentos esperado: {expected_output[0]}, mas encontrou: {total_atendimentos_value}"
+    assert abs(int(normalizado_atendimentos_value) - expected_output[1]) <= 2, f"Valor de Normalizado Atendimentos esperado: {expected_output[1]}, mas encontrou: {normalizado_atendimentos_value}"
+    assert abs(int(big_medicos_value) - expected_output[2]) <= 2, f"Valor de Big Médicos esperado: {expected_output[2]}, mas encontrou: {big_medicos_value}"
