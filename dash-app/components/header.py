340ea82fe39e17a0f7b59e5e0d19cb3172ff1d67
Daniel Schulz <danielfs0@hotmail.com>
2024-11-17 20:48:26 -0300

    Removidos arquivos não utilizados

--- dash-app/components/header.py
+++
@@ -1,5 +0,0 @@
-from dash import html
-
-
-def create_header():
-    return html.H1("Dashboard de Saúde Básica", className="header")
--- dash-app/config/settings.py
+++
@@ -1,9 +0,0 @@
-# config/settings.py
-
-DATABASE_CONFIG = {
-    "dbname": "saude_basica",
-    "user": "saude_basica",
-    "password": "saude_basica",
-    "host": "localhost",
-    "port": 5432,
-}
