644070cc741ca3d0fcba65d3fc338d0a31011b2a
Daniel Schulz <danielfs0@hotmail.com>
2024-12-03 15:24:50 -0300

    Add load backup db

---
+++ load_init_db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Downloading backup"
+# wget -O /tmp/sisab_mongo_init_db_backup.zip https://vpublic-files.s3.us-east-1.amazonaws.com/sisab_mongo_init_db_backup.zip
+
+echo "Unziping backup"
+# unzip /tmp/sisab_mongo_init_db_backup.zip -d /tmp
+
+# rm -f /tmp/sisab_mongo_init_db_backup.zip
+
+echo "Moving backup to container"
+docker cp /tmp/sisab_v2 sisab_database:/tmp
+
+# Executa o comando de restauração no contêiner do MongoDB
+echo "Restoring backup"
+docker exec -it sisab_database mongorestore --uri="mongodb://localhost:27017/sisab_v2" --nsInclude="sisab_v2.*" /tmp/sisab_v2
